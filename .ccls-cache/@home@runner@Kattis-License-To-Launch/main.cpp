ffb6e20d1950e17d0bcacf2dc121a784e789c18f
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-10-12 01:30:21 +0000

    input space junk and cout statement

--- .ccls-cache/@home@runner@Kattis-License-To-Launch/main.cpp
+++ .ccls-cache/@home@runner@Kattis-License-To-Launch/main.cpp
@@ -5,15 +5,20 @@ using namespace std;
 int main() {
 // declaring variables and arrays
   int n; 
-  int daysValid[n];
-  
+
   cin >> n; // number of days license valid
 
+  int daysValid[n];
+
   // loop for input of space junk on each n day
+    for (int i = 0; i < n; i++){
+      cin >> daysValid[i];
+    }
 
   // determine which day (index) has least junk (lowest value)
 
+  
   // output day in index 0
-
+  cout << daysValid[0];
   
 }
