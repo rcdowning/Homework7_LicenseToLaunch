f7d9e4b3d78658a97242e630ec2f3a44d500c4f8
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-10-12 01:25:09 +0000

    problem setup

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+// declaring variables and arrays
+  int n; 
+  int daysValid[n];
+  
+  cin >> n; // number of days license valid
+
+  // loop for input of space junk on each n day
+
+  // determine which day (index) has least junk (lowest value)
+
+  // output day in index 0
+
+  
+}
